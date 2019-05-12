2a03434cf743d23c67feca93b8d7501486534ca7
erick2014 <erickarmandogarcia@hotmail.com>
2019-05-12 16:00:57 -0500

    remove return in Routing

--- src/routing.js
+++ src/routing.js
@@ -10,18 +10,16 @@ import DashboardEventDetail from 'components/dashboard/dashboard.event.edit/dash
 import DashboardEventNew from 'components/dashboard/dashboard.event.new/dashboard.event.new'
 import DashboardEventEdit from 'components/dashboard/dashboard.event.edit/dashboard.event.edit'
 
-const Routing = () => {
-    return (
-        <div>
-            <Route exact path='/' component={Login} />
-            {/* <Route path='/login' component={Login} />
+const Routing = () => (
+  <div>
+    <Route exact path='/' component={Login} />
+    {/* <Route path='/login' component={Login} />
         <Route path='/signup' component={SignUp} />
         <Route path='/dashboard' component={Dashboard} />
         <Route path='/event-detail/:id' component={DashboardEventDetail} />
         <Route path='/event-new/' component={DashboardEventNew} />
         <Route path='/event-edit/:id' component={DashboardEventEdit} /> */}
-        </div>
-    )
-}
+  </div>
+)
 
 export default Routing
