c3c9c85e3364b79a711669125a629fe2677adc04
erick2014 <erickarmandogarcia@hotmail.com>
2019-05-19 14:39:11 -0500

    add dynamic import in webpack and babel

--- src/routing.js
+++ src/routing.js
@@ -1,20 +1,22 @@
 // @vendors
 import React from 'react'
 import { Route } from 'react-router-dom'
+import loadable from '@loadable/component';
 
 // @components
-import Login from 'components/login'
-import SignUp from 'components/signup'
-import Dashboard from 'components/dashboard/dashboard'
-import DashboardEventDetail from 'components/dashboard/dashboard.event.edit/dashboard.event.edit.jsx'
-import DashboardEventNew from 'components/dashboard/dashboard.event.new/dashboard.event.new'
-import DashboardEventEdit from 'components/dashboard/dashboard.event.edit/dashboard.event.edit'
+const LoginComponent = loadable(() => import('./app/components/login'))
+const SignUpComponent = loadable(() => import('./app/components/signup'))
+
+// import Dashboard from 'components/dashboard/dashboard'
+// import DashboardEventDetail from 'components/dashboard/dashboard.event.edit/dashboard.event.edit.jsx'
+// import DashboardEventNew from 'components/dashboard/dashboard.event.new/dashboard.event.new'
+// import DashboardEventEdit from 'components/dashboard/dashboard.event.edit/dashboard.event.edit'
 
 const Routing = () => (
   <div>
-    <Route exact path='/' component={Login} />
-    <Route path='/login' component={Login} />
-    <Route path='/signup' component={SignUp} />
+    <Route exact path='/' component={LoginComponent} />
+    <Route path='/login' component={LoginComponent} />
+    <Route path='/signup' component={SignUpComponent} />
     {/* // <Route path='/dashboard' component={Dashboard} />
         // <Route path='/event-detail/:id' component={DashboardEventDetail} />
         // <Route path='/event-new/' component={DashboardEventNew} />
