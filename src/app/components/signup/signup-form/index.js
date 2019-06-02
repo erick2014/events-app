891b8444caae867d412d3bf7020cbd36bc9753b7
erick2014 <erickarmandogarcia@hotmail.com>
2019-06-01 19:56:38 -0500

    refactor layout for signup page

--- src/app/components/signup/signup-form/index.js
+++ src/app/components/signup/signup-form/index.js
@@ -1,250 +1,236 @@
 // @vendors
-import React, { Component } from 'react'
-import { withStyles } from '@material-ui/core/styles'
-import Grid from '@material-ui/core/Grid'
-import { Link } from 'react-router-dom'
-import { FormControl, FormHelperText } from '@material-ui/core'
-
-// util functions
-import utils from 'utils/utils'
+import React, { Component, Fragment } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import { Link } from 'react-router-dom';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Input from '@material-ui/core/Input';
 
 // styles to override in components
 import inputStyles from 'components/with.styles/input'
 
 // components
-import CustomButton from 'components/custom.button/custom.button'
+import CustomButton from '../../custom-button';
+import TwoColumnLayout from '../../two-column-layout';
+
+// util functions
+import utils from '../../../utils/utils';
+
+import './style.scss';
 
 class SignUpForm extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
-      repeatPassword: '',
-      showPassword: false,
-      showRepeatPassword: false,
-      errors: {
-        'firstName': { error: false, message: '' },
-        'lastName': { error: false, message: '' },
-        'email': { error: false, message: '' },
-        'password': { error: false, message: '' },
-        'repeatPassword': { error: false, message: '' }
-      }
+    state = {
+        email: '',
+        errors: {
+            firstName: { error: false, message: '' },
+            lastName: { error: false, message: '' },
+            email: { error: false, message: '' },
+            password: { error: false, message: '' },
+            repeatPassword: { error: false, message: '' }
+        },
+        errorInSignUp: false,
+        fields: {
+            firstName: '',
+            lastName: '',
+            password: '',
+            repeatPassword: ''
+        },
+        showPassword: false,
+        showRepeatPassword: false,
+        successSignUp: false
     }
 
-    this.onClickSignUpBtn = this.onClickSignUpBtn.bind(this)
-    this.handleClickShowPasssword = this.handleClickShowPasssword.bind(this)
-    this.handleMouseDownPassword = this.handleMouseDownPassword.bind(this)
-  }
-
-  handleClickShowPasssword(field) {
-    this.setState({ [field]: !this.state[field] })
-  }
-
-  handleMouseDownPassword(event) {
-    event.preventDefault()
-  };
-
-  handleInputChange(inputName, event, type = '') {
-    let validField = utils.fieldIsValid(inputName, event.target.value, type)
-
-    let newStateErrors = Object.assign({}, this.state.errors, {
-      [inputName]: { error: validField[inputName], message: validField['message'] }
-    })
-    // update the state with the values and errors
-    this.setState({
-      [inputName]: event.target.value,
-      'errors': newStateErrors
-    })
-    this.setState({ [inputName]: event.target.value })
-  }
-
-  onClickSignUpBtn() {
-    const { signUp, signUpFailure } = this.props
-    const fieldsToCheck = {
-      firstName: this.state.firstName,
-      lastName: this.state.lastName,
-      email: this.state.email,
-      password: this.state.password,
-      repeatPassword: this.state.repeatPassword
+    handleClickShowPasssword = (fieldName) => {
+        const { [fieldName]: field } = this.state;
+        this.setState({ [field]: !field });
     }
-    const fieldsValid = utils.fieldsAreValid(fieldsToCheck, this.state.errors)
 
-    if (fieldsValid) {
-      // dispatch this action to sign up a new user
-      signUp({
-        firstName: this.state.firstName,
-        lastName: this.state.lastName,
-        email: this.state.email,
-        password: this.state.password
-      })
-    } else {
-      // dispatch this action to show up a validation message in the ui
-      signUpFailure('All fields with * are required')
+    handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    signUpUser = () => {
+        // @todo implement
     }
-  }
 
-  buildFillingText() {
-    const { users: { error, errorMessage, success, successMessage } } = this.props
-    let componentToRender
-    if (!error && !success) {
-      componentToRender = <div className='sign-up-form__right-column-fill-in-text'>Enter your details below</div>
-    } else if (success && !error && successMessage !== '') {
-      componentToRender = <div className='sign-up-form__right-column-fill-in-text'>{successMessage}</div>
-    } else {
-      componentToRender = <div className='sign-up-form__right-column-fill-in-text--red'>{errorMessage}</div>
+    signUpFailure = () => {
+        // @todo implement
     }
-    return componentToRender
-  }
 
-  render() {
-    const { classes } = this.props
-    const fillInText = this.buildFillingText()
-    const { firstName: errFirstName, lastName: errLastName, email: errEmail, password: errPassword, repeatPassword: errRepeatedPass } = this.state.errors
+    onClickSignUpBtn = () => {
+        const {
+            email,
+            errors,
+            firstName,
+            lastName,
+            password,
+            repeatPassword
+        } = this.state;
 
-    return (
-      <Grid container className='sign-up-form' spacing={24}>
-        <Grid item md={3} className='sign-up-form__left-column'>
-          <div className='sign-up-form__left-column-bg-gray'>
-            <div className='sign-up-form__left-column-text'>
-              <div>"Great, kid Don't get cocky."</div>
-              <div className='sign-up-form__left-column-text--green'>-</div>
-              <div>Han solo</div>
+        const fieldsToCheck = {
+            firstName,
+            lastName,
+            email,
+            password,
+            repeatPassword
+        };
+
+        const fieldsValid = utils.fieldsAreValid(fieldsToCheck, errors);
+
+        if (fieldsValid) {
+            // dispatch this action to sign up a new user
+            this.signUpUser({
+                firstName,
+                lastName,
+                email,
+                password
+            });
+        } else {
+            this.signUpFailure('All fields with * are required');
+        }
+    }
+
+    handleInputChange(inputName, { target }, type = '') {
+        const { errors } = this.state;
+        const fieldValue = target.value;
+        const validField = utils.fieldIsValid(inputName, fieldValue, type);
+
+        const newStateErrors = Object.assign({}, errors, {
+            [inputName]: { error: validField[inputName], message: validField.message }
+        });
+        // update the state with the values and errors
+        this.setState({
+            [inputName]: fieldValue,
+            errors: newStateErrors
+        });
+    }
+
+    buildSecondTitleText() {
+        const { errorInSignUp, successSignUp } = this.state;
+
+        if (!errorInSignUp && !successSignUp) {
+            return <div className="second-title">Enter your details below</div>;
+        }
+
+        if (!errorInSignUp && successSignUp) {
+            return <div className="second-title">server success message!</div>;
+        }
+
+        return <div className="second-title">server error!</div>;
+    }
+
+    renderFormFields() {
+        const {
+            errors: {
+                email: errEmail,
+                firstName: errFirstName,
+                lastName: errLastName,
+                password: errPassword,
+                repeatPassword: errRepeatedPass
+            },
+            fields: {
+                email,
+                firstName,
+                lastName,
+                password,
+                repeatPassword
+            },
+            showPassword,
+            showRepeatPassword
+        } = this.state;
+
+        const { classes } = this.props;
+
+        return (
+            <Fragment>
+                <div className="first-name-container">
+                    <TextField
+                        className="text-field-container"
+                        error={errFirstName.error}
+                        id="firstName"
+                        label="First Name"
+                        margin="normal"
+                        onChange={event => this.handleInputChange('email', event)}
+                        value={firstName}
+                    />
+                </div>
+                <div className="lastname-container">
+                    <TextField
+                        className="text-field-container"
+                        error={errLastName.error}
+                        id="lastName"
+                        label="Last name"
+                        margin="normal"
+                        onChange={event => this.handleChange('lastName', event)}
+                        value={lastName}
+                    />
+                </div>
+                <div className="lastname-container">
+                    <TextField
+                        className="text-field-container"
+                        error={errEmail.error}
+                        id="email"
+                        label="Email"
+                        margin="normal"
+                        onChange={event => this.handleChange('email', event)}
+                        value={email}
+                    />
+                </div>
+                <div className="lastname-container">
+                    <TextField
+                        className="text-field-container"
+                        error={errPassword.error}
+                        id="password"
+                        label="Password"
+                        margin="normal"
+                        onChange={event => this.handleChange('password', event)}
+                        value={password}
+                    />
+                </div>
+                <div className="lastname-container">
+                    <TextField
+                        className="text-field-container"
+                        error={errRepeatedPass.error}
+                        id="lastName"
+                        label="Repeat password"
+                        margin="normal"
+                        onChange={event => this.handleChange('repeatPassword', event)}
+                        value={repeatPassword}
+                    />
+                </div>
+            </Fragment>
+        );
+    }
+
+    render() {
+        const topLinkTitle = (
+            <div className="sign-up-link">
+                <Link to="/signup">Already have an account? SIGN IN </Link>
             </div>
-          </div>
-        </Grid>
-        <Grid item md={9} className='sign-up-form__right-column'>
-          <div>
-            <div className='sign-up-form__right-column-signin-text'>
-              <Link to='/'>Already have an account? SIGN IN </Link>
+        );
+
+        const secondTitle = this.buildSecondTitleText();
+        const formTitles = (
+            <div className="main-titles-container">
+                <div className="title">Get started absolutely free.</div>
+                {secondTitle}
             </div>
-            <div className='sign-up-form__right-column-form-fields'>
-              <form>
+        );
 
-                <div className='sign-up-form__right-column-title'>
-                  <h1>
-                    Get started absolutely free.
-                  </h1>
-                  {fillInText}
-                </div>
+        const formFields = this.renderFormFields();
 
-                <div className='sign-up-form__right-column-input'>
-                  <FormControl
-                    fullWidth
-                    margin='normal'
-                    required
-                    error={errFirstName.error}
-                  >
-                    <div classes={{ root: classes.rootLabel }}>
-                      First name
-                    </div>
-                    <input type="text"
-                      classes={{ inkbar: classes.inkbar, error: classes.error }}
-                      value={this.state.firstName}
-                      onChange={event => this.handleInputChange('firstName', event)}
-                    />
-                    <FormHelperText>
-                      {errFirstName ? errFirstName.message : ''}
-                    </FormHelperText>
-                  </FormControl>
-                </div>
-
-                <div className='sign-up-form__right-column-input'>
-                  <FormControl
-                    className={classes.formControl}
-                    fullWidth
-                    margin='normal'
-                    required
-                    error={errLastName.error}
-                  >
-                    <div classes={{ root: classes.rootLabel }}>
-                      Last name
-                    </div>
-                    <input type="text"
-                      classes={{ inkbar: classes.inkbar, error: classes.error }}
-                      value={this.state.lastName}
-                      onChange={event => this.handleInputChange('lastName', event)}
-                    />
-                    <FormHelperText>
-                      {errLastName ? errLastName.message : ''}
-                    </FormHelperText>
-                  </FormControl>
-                </div>
-
-                <div className='sign-up-form__right-column-input'>
-                  <FormControl
-                    className={classes.formControl}
-                    fullWidth
-                    margin='normal'
-                    required
-                    error={errEmail.error}
-                  >
-                    <div classes={{ root: classes.rootLabel }}>
-                      Email
-                    </div>
-                    <input type="text"
-                      classes={{ inkbar: classes.inkbar, error: classes.error }}
-                      value={this.state.email}
-                      onChange={event => this.handleInputChange('email', event, 'email')}
-                    />
-                    <FormHelperText>
-                      {errEmail ? errEmail.message : ''}
-                    </FormHelperText>
-                  </FormControl>
-                </div>
-
-                <div className='sign-up-form__right-column-input'>
-                  <FormControl
-                    className={classes.formControl}
-                    error={errPassword.error}
-                    margin='normal'
-                    fullWidth>
-                    <div htmlFor='password'>Password</div>
-                    <input
-                      classes={{ inkbar: classes.inkbar, error: classes.error }}
-                      type={this.state.showPassword ? 'text' : 'password'}
-                      value={this.state.password}
-                      onChange={event => this.handleInputChange('password', event)}
-                    />
-                    <FormHelperText>
-                      {errPassword ? errPassword.message : ''}
-                    </FormHelperText>
-                  </FormControl>
-                </div>
-
-                <div className='sign-up-form__right-column-input'>
-                  <FormControl
-                    className={classes.formControl}
-                    error={errRepeatedPass.error}
-                    margin='normal'
-                    fullWidth>
-                    <div htmlFor='password'>Password</div>
-                    <input
-                      classes={{ inkbar: classes.inkbar, error: classes.error }}
-                      type={this.state.showRepeatPassword ? 'text' : 'password'}
-                      value={this.state.repeatPassword}
-                      onChange={event => this.handleInputChange('repeatPassword', event)}
-                    />
-                    <FormHelperText>
-                      {errRepeatedPass ? errRepeatedPass.message : ''}
-                    </FormHelperText>
-                  </FormControl>
-
-                </div>
-
-                <div className='login-form__right-column-submit-btn'>
-                  <CustomButton text={'SIGN UP'} onClickHandler={this.onClickSignUpBtn} />
-                </div>
-
-              </form>
+        return (
+            <div className="signup-page">
+                <TwoColumnLayout>
+                    {topLinkTitle}
+                    {formTitles}
+                    {formFields}
+                    <CustomButton onClickHandler={this.onClickSignUpBtn} text="SIGN IN" />
+                </TwoColumnLayout>
             </div>
-          </div>
-        </Grid>
-      </Grid>
-    )
-  }
+        );
+    }
 }
 // bind component to the store
-export default withStyles(inputStyles)(SignUpForm)
+export default withStyles(inputStyles)(SignUpForm);
